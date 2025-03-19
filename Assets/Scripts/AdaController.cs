e523c4e96d30cc74dd86fdbbb8633035fa9776ca
Diana <42554824+JUPiTERdia117@users.noreply.github.com>
2025-03-19 16:49:32 -0600

    Ada prototype upd
    Se actualiza prototipo de ada. Ya crece y se encoge de tamaño. Hay margen de error con la vida al reducirse.

--- Assets/Scripts/AdaController.cs
+++ Assets/Scripts/AdaController.cs
@@ -6,40 +6,66 @@ using UnityEngine.Rendering.Universal;
 public class AdaController : MonoBehaviour
 {
     [SerializeField] int vida = 1;
+   
 
     Light2D light2D; // Asigna tu luz en el inspector
     float minSize = 1f;  // Tamaño mínimo del Outer Radius
     [SerializeField] float maxSize=15.0f;  // Tamaño máximo del Outer Radius
-    [SerializeField] float growthSpeed=1.0f; // Velocidad de crecimiento/reducción
+    float targetSize; // Tamaño objetivo
+    public float currentSize; // Tamaño anterior
+    [SerializeField] float growthSpeed, shrinkSpeed; // Velocidad de crecimiento/reducción
     bool creciendo = false; // Condición que determina si crece o se reduce
+
+    Coroutine golpeCoroutine;
 
     [SerializeField] float tToHit=1.0f;
 
     void Awake(){
         light2D = GetComponent<Light2D>();
         minSize = light2D.pointLightOuterRadius;
+        targetSize = light2D.pointLightOuterRadius;
     }
 
     // Update is called once per frame
     void Update()
     {
+        currentSize = light2D.pointLightOuterRadius;
         if(creciendo){
-            light2D.pointLightOuterRadius = Mathf.Lerp(light2D.pointLightOuterRadius, maxSize, Time.deltaTime * growthSpeed);
+            if(light2D.pointLightOuterRadius < maxSize){
+                light2D.pointLightOuterRadius = Mathf.Lerp(light2D.pointLightOuterRadius, targetSize, Time.deltaTime * growthSpeed);
+               
+            }
 
-        }else{
-            light2D.pointLightOuterRadius = Mathf.Lerp(light2D.pointLightOuterRadius, minSize, Time.deltaTime * growthSpeed);
+
         }
+        
+        if(!creciendo && light2D.pointLightOuterRadius > minSize){
+            light2D.pointLightOuterRadius = Mathf.Lerp(light2D.pointLightOuterRadius, minSize, Time.deltaTime * shrinkSpeed);
+
+            if(light2D.pointLightOuterRadius%(maxSize/20) < 0.5f && vida>1){
+                vida--;
+                
+            }
+        }
+
+        
         
     }
 
     public bool DarVida(){
+        targetSize+=maxSize/20;
         creciendo = true;
         vida++;
-        //transform.localScale = new Vector2(transform.localScale.x+1,transform.localScale.y+1);
-        
-        StartCoroutine(EsperarGolpe(tToHit));
 
-        if(vida==4 && light2D.pointLightOuterRadius>10.0f){
+        
+        
+        if (golpeCoroutine != null) {
+            StopCoroutine(golpeCoroutine);
+        }
+        
+        golpeCoroutine = StartCoroutine(EsperarGolpe(tToHit));
+
+        if(vida==20){
             return true;
         }
         else{
@@ -52,8 +78,8 @@ public class AdaController : MonoBehaviour
         
      
         yield return new WaitForSeconds(segundos);
-
         creciendo = false;
+
     }
 
 }
