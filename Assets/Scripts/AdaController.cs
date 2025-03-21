866a4bc0a855820c14563ea07fbbbde665fd6e94
Diana Monroy <42554824+JUPiTERdia117@users.noreply.github.com>
2025-03-20 18:55:38 -0600

    Ada Comentarios & Upd
    Se actualiza ligeramente Scritp. Se añaden comentarios

--- Assets/Scripts/AdaController.cs
+++ Assets/Scripts/AdaController.cs
@@ -3,24 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
+// Script para controlar a Ada
 public class AdaController : MonoBehaviour
 {
-    [SerializeField] int vida = 1;
+    [SerializeField] int vida = 1; // Vida inicial de Ada
    
 
     Light2D light2D; // Asigna tu luz en el inspector
     float minSize = 1f;  // Tamaño mínimo del Outer Radius
     [SerializeField] float maxSize=15.0f;  // Tamaño máximo del Outer Radius
     float targetSize; // Tamaño objetivo
-    public float currentSize; // Tamaño anterior
+    //public float currentSize; // Tamaño anterior
     [SerializeField] float growthSpeed, shrinkSpeed; // Velocidad de crecimiento/reducción
-    public bool creciendo = false; // Condición que determina si crece o se reduce
+    bool creciendo = false; // Condición que determina si crece o se reduce
 
-    Coroutine golpeCoroutine;
+    Coroutine golpeCoroutine; // Corrutina para esperar el golpe
 
-    [SerializeField] float tToHit=1.0f;
+    [SerializeField] float tToHit=1.0f; // Tiempo para recibir un golpe
 
     void Awake(){
+        // Asignar la luz
         light2D = GetComponent<Light2D>();
         minSize = light2D.pointLightOuterRadius;
         targetSize = light2D.pointLightOuterRadius;
@@ -29,7 +31,8 @@ public class AdaController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        currentSize = light2D.pointLightOuterRadius;
+        //currentSize = light2D.pointLightOuterRadius;
+        //Si está en estado de crecimiento y no ha alcanzado el tamaño máximo
         if(creciendo){
             if(light2D.pointLightOuterRadius < maxSize){
                 light2D.pointLightOuterRadius = Mathf.Lerp(light2D.pointLightOuterRadius, targetSize, Time.deltaTime * growthSpeed);
@@ -39,7 +42,8 @@ public class AdaController : MonoBehaviour
 
         }
         
-        if(!creciendo && light2D.pointLightOuterRadius > minSize){
+        //Si está en estado de reducción y no ha alcanzado el tamaño mínimo
+        if(!creciendo){
             targetSize = light2D.pointLightOuterRadius;
             light2D.pointLightOuterRadius = Mathf.Lerp(light2D.pointLightOuterRadius, minSize, Time.deltaTime * shrinkSpeed);
 
@@ -53,19 +57,21 @@ public class AdaController : MonoBehaviour
         
     }
 
+    // Método para dar vida a Ada
     public bool DarVida(){
         targetSize+=maxSize/20;
         creciendo = true;
         vida++;
 
         
-        
+        //Si ya hay una corrutina en espera, detenerla
         if (golpeCoroutine != null) {
             StopCoroutine(golpeCoroutine);
         }
-        
+        //Iniciar la corrutina para esperar el golpe
         golpeCoroutine = StartCoroutine(EsperarGolpe(tToHit));
 
+        //Si la vida es igual a 20, devolver verdadero para ganar
         if(vida==20){
             return true;
         }
@@ -75,6 +81,7 @@ public class AdaController : MonoBehaviour
 
     }
 
+    //Corrutina para esperar el golpe
     private IEnumerator EsperarGolpe(float segundos){
         
      
